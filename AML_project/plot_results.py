f43b312f6e28219f6edee31d5449b1b16bf4f877
guustoussaint <guustouss@gmail.com>
2021-12-17 16:31:39 +0100

    changed plot and reran expr. 3

--- AML_project/plot_results.py
+++ AML_project/plot_results.py
@@ -113,135 +113,148 @@ def normalized_plot(problems, get_loss_from_run_fn=lambda r: r.loss, cmap=plt.ge
             best_normalized_losses[i].append(min_normalized_loss)
             longest_normalized_times[i].append(max_norm_loss)
 
-    fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-    for i, j in enumerate(["randomsearch", "randomforest", "BOHB"]):
-        normalized_data[j] = np.array(normalized_data[j])
-        ax.scatter(normalized_data[j][:, 0], normalized_data[j][:, 1], color=cmap(i), label=j, alpha=0.3)
+        for i, j in enumerate(["randomsearch", "randomforest", "BOHB"]):
+            normalized_data[j] = np.array(normalized_data[j])
+            # ax.scatter(normalized_data[j][:, 0], normalized_data[j][:, 1], color=cmap(i), label=j, alpha=0.3)
 
-        # ax.step(data[j][:, 0], np.minimum.accumulate(
-        #     data[j][:, 1]), where='post')
+            x = []
+            y = []
+            min = np.inf
+            sorted_vals = np.argsort(normalized_data[j][:, 0])
+            print(sorted_vals)
+            for p in sorted_vals:
+                d = normalized_data[j][p]
+                if d[1] < min:
+                    min = d[1]
+                    x.append(d[0])
+                    y.append(d[1])
 
-    # Scatter normalized
-    ax.set_title('Normalized loss over normalized time for all runs')
-    ax.set_xlabel('wall clock time (normalized per problem)')
-    ax.set_ylabel('loss (normalized per problem)')
-    ax.legend()
-    plt.show()
+            ax.plot(x, y, "o-", label=j)
 
+        # Scatter normalized
+        ax.set_title('Normalized loss over normalized time for all runs')
+        ax.set_xlabel('wall clock time (normalized per problem)')
+        ax.set_ylabel('loss (normalized per problem)')
+        ax.legend()
+        plt.show()
 
-    # Boxplot all
-    randomforest_data = np.array(normalized_data['randomforest'])
-    randomsearch_data = np.array(normalized_data['randomsearch'])
-    bohb_data = np.array(normalized_data['BOHB'])
+        break
 
-    boxplot_time = [randomsearch_data[:, 0], randomforest_data[:, 0], bohb_data[:, 0]]
-    fig, ax = plt.subplots()
-    ax.boxplot(boxplot_time, labels=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized wall clock time for all runs')
-    ax.set_xlabel('Method')
-    ax.set_ylabel('Wall clock time (normalized per problem)')
-    plt.show()
+        # Boxplot all
+        randomforest_data = np.array(normalized_data['randomforest'])
+        randomsearch_data = np.array(normalized_data['randomsearch'])
+        bohb_data = np.array(normalized_data['BOHB'])
 
-    boxplot_loss = [randomsearch_data[:, 1], randomforest_data[:, 1], bohb_data[:, 1]]
-    fig, ax = plt.subplots()
-    ax.boxplot(boxplot_loss, labels=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized loss for all runs')
-    ax.set_xlabel('Method')
-    ax.set_ylabel('Loss (normalized per problem)')
-    plt.show()
+        boxplot_time = [randomsearch_data[:, 0], randomforest_data[:, 0], bohb_data[:, 0]]
+        fig, ax = plt.subplots()
+        ax.boxplot(boxplot_time, labels=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized wall clock time for all runs')
+        ax.set_xlabel('Method')
+        ax.set_ylabel('Wall clock time (normalized per problem)')
+        plt.show()
 
-
-
-    # Boxplot best and longest
-    boxplot_time_longest = [longest_normalized_times['randomsearch'], longest_normalized_times['randomforest'], longest_normalized_times['BOHB']]
-    fig, ax = plt.subplots()
-
-    ax.boxplot(boxplot_time_longest, labels=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized longest wall clock time')
-    ax.set_xlabel('Method')
-    ax.set_ylabel('Wall clock time (normalized per problem)')
-    plt.show()
-
-    boxplot_loss_best = [best_normalized_losses['randomsearch'], best_normalized_losses['randomforest'], best_normalized_losses['BOHB']]
-    fig, ax = plt.subplots()
-    ax.boxplot(boxplot_loss_best, labels=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized best loss')
-    ax.set_xlabel('Method')
-    ax.set_ylabel('Loss (normalized per problem)')
-    plt.show()
+        boxplot_loss = [randomsearch_data[:, 1], randomforest_data[:, 1], bohb_data[:, 1]]
+        fig, ax = plt.subplots()
+        ax.boxplot(boxplot_loss, labels=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized loss for all runs')
+        ax.set_xlabel('Method')
+        ax.set_ylabel('Loss (normalized per problem)')
+        plt.show()
 
 
-    # Histogram all
-    fig, ax = plt.subplots()
-    ax.hist(boxplot_loss, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized loss for all runs')
-    ax.set_xlabel('Loss (normalized per problem)')
-    ax.legend()
-    plt.show()
 
-    fig, ax = plt.subplots()
-    ax.hist(boxplot_time, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized wall clock time for all runs')
-    ax.set_xlabel('Wall clock time (normalized per problem)')
-    ax.legend()
-    plt.show()
+        # Boxplot best and longest
+        boxplot_time_longest = [longest_normalized_times['randomsearch'], longest_normalized_times['randomforest'], longest_normalized_times['BOHB']]
+        fig, ax = plt.subplots()
 
-    #Histogram best and longest
-    fig, ax = plt.subplots()
-    ax.hist(boxplot_loss_best, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized best loss')
-    ax.set_xlabel('Loss (normalized per problem)')
-    ax.legend()
-    plt.show()
+        ax.boxplot(boxplot_time_longest, labels=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized longest wall clock time')
+        ax.set_xlabel('Method')
+        ax.set_ylabel('Wall clock time (normalized per problem)')
+        plt.show()
 
-    fig, ax = plt.subplots()
-    ax.hist(boxplot_time_longest, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
-    ax.set_title('Normalized longest wall clock time')
-    ax.set_xlabel('Wall clock time (normalized per problem)')
-    ax.legend()
-    plt.show()
+        boxplot_loss_best = [best_normalized_losses['randomsearch'], best_normalized_losses['randomforest'], best_normalized_losses['BOHB']]
+        fig, ax = plt.subplots()
+        ax.boxplot(boxplot_loss_best, labels=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized best loss')
+        ax.set_xlabel('Method')
+        ax.set_ylabel('Loss (normalized per problem)')
+        plt.show()
 
 
-    # Rank plot
-    fig, ax = plt.subplots()
-    ranks = np.arange(3)
-    bar_width = 0.9/len(ranks)
+        # Histogram all
+        fig, ax = plt.subplots()
+        ax.hist(boxplot_loss, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized loss for all runs')
+        ax.set_xlabel('Loss (normalized per problem)')
+        ax.legend()
+        plt.show()
 
-    # print(np.bincount(loss_ranks['randomsearch'])[1:])
-    ax.bar(ranks, np.bincount(loss_ranks['randomsearch'])[1:], bar_width, label='Random search')
-    ax.bar(ranks + bar_width, np.bincount(loss_ranks['randomforest'])[1:], bar_width, label='BOHB-RF')
-    ax.bar(ranks + 2 * bar_width, np.bincount(loss_ranks['BOHB'])[1:], bar_width, label='BOHB')
-    
+        fig, ax = plt.subplots()
+        ax.hist(boxplot_time, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized wall clock time for all runs')
+        ax.set_xlabel('Wall clock time (normalized per problem)')
+        ax.legend()
+        plt.show()
 
-    # Prints some stats
-    plt.xlabel("Rank")
-    plt.ylabel("Number of occurences")
-    plt.xticks(ranks + bar_width, ('1', '2', '3'))
-    plt.legend()
-    plt.show()
+        #Histogram best and longest
+        fig, ax = plt.subplots()
+        ax.hist(boxplot_loss_best, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized best loss')
+        ax.set_xlabel('Loss (normalized per problem)')
+        ax.legend()
+        plt.show()
 
-    print("Average randomsearch normalized best loss:", np.mean(best_normalized_losses['randomsearch']))
-    print("Average BOHB-RF normalized best loss:", np.mean(best_normalized_losses['randomforest']))
-    print("Average BOHB normalized best loss:", np.mean(best_normalized_losses['BOHB']), "\n")
+        fig, ax = plt.subplots()
+        ax.hist(boxplot_time_longest, bins=10, density=False, histtype='bar', label=["Randomsearch", "BOHB-RF", "BOHB"])
+        ax.set_title('Normalized longest wall clock time')
+        ax.set_xlabel('Wall clock time (normalized per problem)')
+        ax.legend()
+        plt.show()
 
-    # print("Average randomsearch normalized wall clock time:", np.mean(randomsearch_data[:, 0]))
-    # print("Average BOHB-RF normalized wall clock time:", np.mean(randomforest_data[:, 0]))
-    # print("Average BOHB normalized wall clock time:", np.mean(bohb_data[:, 0]), "\n")
 
-    print("Average randomsearch normalized wall clock time:", np.mean(longest_normalized_times['randomsearch']))
-    print("Average BOHB-RF normalized wall clock time:", np.mean(longest_normalized_times['randomforest']))
-    print("Average BOHB normalized wall clock time:", np.mean(longest_normalized_times['BOHB']), "\n")
+        # Rank plot
+        fig, ax = plt.subplots()
+        ranks = np.arange(3)
+        bar_width = 0.9/len(ranks)
 
-    print("Average randomsearch rank:", np.mean(loss_ranks['randomsearch']))
-    print("Average BOHB-RF rank:", np.mean(loss_ranks["randomforest"]))
-    print("Average BOHB rank:", np.mean(loss_ranks["BOHB"]))
+        # print(np.bincount(loss_ranks['randomsearch'])[1:])
+        ax.bar(ranks, np.bincount(loss_ranks['randomsearch'])[1:], bar_width, label='Random search')
+        ax.bar(ranks + bar_width, np.bincount(loss_ranks['randomforest'])[1:], bar_width, label='BOHB-RF')
+        ax.bar(ranks + 2 * bar_width, np.bincount(loss_ranks['BOHB'])[1:], bar_width, label='BOHB')
+        
 
-    print("BOHB-RF duration in seconds:", bohbrf_time)
+        # Prints some stats
+        plt.xlabel("Rank")
+        plt.ylabel("Number of occurences")
+        plt.xticks(ranks + bar_width, ('1', '2', '3'))
+        plt.legend()
+        plt.show()
 
-    return(fig, ax)
+        print("Average randomsearch normalized best loss:", np.mean(best_normalized_losses['randomsearch']))
+        print("Average BOHB-RF normalized best loss:", np.mean(best_normalized_losses['randomforest']))
+        print("Average BOHB normalized best loss:", np.mean(best_normalized_losses['BOHB']), "\n")
 
-problems = [3, 6, 11, 12, 14, 16, 18, 20, 21, 22, 23, 28, 43, 45, 49, 53, 58, 219, 2074]
+        # print("Average randomsearch normalized wall clock time:", np.mean(randomsearch_data[:, 0]))
+        # print("Average BOHB-RF normalized wall clock time:", np.mean(randomforest_data[:, 0]))
+        # print("Average BOHB normalized wall clock time:", np.mean(bohb_data[:, 0]), "\n")
+
+        print("Average randomsearch normalized wall clock time:", np.mean(longest_normalized_times['randomsearch']))
+        print("Average BOHB-RF normalized wall clock time:", np.mean(longest_normalized_times['randomforest']))
+        print("Average BOHB normalized wall clock time:", np.mean(longest_normalized_times['BOHB']), "\n")
+
+        print("Average randomsearch rank:", np.mean(loss_ranks['randomsearch']))
+        print("Average BOHB-RF rank:", np.mean(loss_ranks["randomforest"]))
+        print("Average BOHB rank:", np.mean(loss_ranks["BOHB"]))
+
+        print("BOHB-RF duration in seconds:", bohbrf_time)
+
+        return(fig, ax)
+
+# problems = [3, 6, 11, 12, 14, 16, 18, 20, 21, 22, 23, 28, 43, 45, 49, 53, 58, 219, 2074]
+problems = [3]
 # print(len(problems))
 
 normalized_plot(problems=problems)
